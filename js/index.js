eb310bc4f6ca48c52d826d2eb9fbbdd5db89fe90
Brianna Smith <smith4bl@dukes.jmu.edu>
2017-04-27 23:51:12 -0400

    finalized index.js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// define functions
+var month = ["January", "February", "March", "April", "May", "June", "July", 
+				"August","September", "October", "November", "December"];
+ var getDaysInMonth = function(month) {
+	if ( month == "January" || month == "March" || month == "May" || month == "July" || month == "August" || month == "October" || month =="December") {
+		return 31;
+	} 
+	else if (month == "April" || month == "June" || month == "September" || month == "November") {
+		return 30;
+	} 
+	else {
+		return 28;
+	}
+};
+
+module.exports = {
+	getDaysInMonth: getDaysInMonth
+};
--- js/spec/collections_spec.js
+++ js/spec/collections_spec.js
@@ -3,5 +3,21 @@ var collections = require("../index");
 
 describe("A program to demonstrate the concept of collections", () => {
 
-	it("can ")
-}); 
+	it("can report if a given month, during a common year, has how many number of days", () => {
+		let month = ["January", "February", "March", "April", "May", "June", "July", 
+				"August","September", "October", "November", "December"];
+		var getDaysInMonth = require("../index");
+		expect(collections.getDaysInMonth("January")).toBe(31);
+		expect(collections.getDaysInMonth("February")).toBe(28);
+		expect(collections.getDaysInMonth("March")).toBe(31);
+		expect(collections.getDaysInMonth("April")).toBe(30);
+		expect(collections.getDaysInMonth("May")).toBe(31);
+		expect(collections.getDaysInMonth("June")).toBe(30);
+		expect(collections.getDaysInMonth("July")).toBe(31);
+		expect(collections.getDaysInMonth("August")).toBe(31);
+		expect(collections.getDaysInMonth("September")).toBe(30);
+		expect(collections.getDaysInMonth("October")).toBe(31);
+		expect(collections.getDaysInMonth("November")).toBe(30);
+		expect(collections.getDaysInMonth("December")).toBe(31);
+	}); 
+});
